e4395b19e44a8502111d08232201bb0a8c7998a3
AnuragOP01 <121534840+AnuragOP01@users.noreply.github.com>
2024-09-27 05:08:24 +0000

    commit

--- server/registrations/views.py
+++ server/registrations/views.py
@@ -6,7 +6,17 @@ from .serializers import RegistrationSerializer
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from django.core.mail import send_mail
+from django.conf import settings
 
+def send_confirmation_email(user_email):
+   send_mail(
+      'Webinar Registration Confirmation',
+      'Thank you for registering for our webinar!',
+      settings.EMAIL_HOST_USER,
+      [user_email],
+      fail_silently=False,
+      )
 
 def register_webinar(request):
  if request.method == 'POST':
@@ -14,6 +24,9 @@ def register_webinar(request):
     if form.is_valid():
         form.save()
         # Redirect or show a success message
+        user_email = form.cleaned_data.get('email')  # Get the registered user's email
+        # print(user_email)
+        send_confirmation_email(user_email)
         return redirect('success_page')
  else:
     form = RegistrationForm()
@@ -36,12 +49,18 @@ class WebinarRegistrationAPI(APIView):
     def post(self, request, *args, **kwargs):
         serializer = RegistrationSerializer(data=request.data)
         if serializer.is_valid():
-            # Save the valid data to the database
+            # Save the valid data to the database    
             serializer.save()
+
+            # Send confirmation email to the email provided in the API data
+            user_email = serializer.data.get('email')
+            print(' Email  ', user_email)
+            send_confirmation_email(user_email)
+           
             # Return a success message and the saved data in the response
             return Response({
                 'message': 'Registration successful',
-                'data': serializer.data
+                'data': {**serializer.data, 'message': 'Registered'},  # Merging dictionaries
             }, status=status.HTTP_201_CREATED)
 
         # If the data is not valid, return errors with a 400 Bad Request status
@@ -50,3 +69,8 @@ class WebinarRegistrationAPI(APIView):
             'errors': serializer.errors,
             'data': serializer.data
         }, status=status.HTTP_400_BAD_REQUEST)
+    
+
+
+
+
