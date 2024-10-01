b9f05190c4515d669b9d6f117f6fd7a39ac1a49c
sansh72 <153851665+sansh72@users.noreply.github.com>
2024-10-01 23:31:00 +0530

    Update Login.jsx

--- client/src/components/ui/Login/Login.jsx
+++ client/src/components/ui/Login/Login.jsx
@@ -25,7 +25,7 @@ const Login = () => {
     e.preventDefault();
     setError(null);
 
-    const response = await fetch('http://localhost:8000/login/', {
+    const response = await fetch('https://django-1-oiac.onrender.com/login/', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
