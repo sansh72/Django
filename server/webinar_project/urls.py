80a88eb53381191e4a30167ef034b6397eb0aa8e
sansh72 <153851665+sansh72@users.noreply.github.com>
2024-10-02 17:35:00 +0530

    Update urls.py

--- server/webinar_project/urls.py
+++ server/webinar_project/urls.py
@@ -1,10 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/registrations/', include('registrations.urls')),
-    path('api/authentication/', include('authentication.urls')),
+    path('', include('registrations.urls')),
+    path('', include('authentication.urls')),
 ]
-
-
