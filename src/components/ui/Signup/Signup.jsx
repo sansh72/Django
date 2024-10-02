c2da1ffe830abf193fee158504af1bc79e3a675c
sansh72 <153851665+sansh72@users.noreply.github.com>
2024-10-02 15:11:40 +0530

    Update Signup.jsx

--- src/components/ui/Signup/Signup.jsx
+++ src/components/ui/Signup/Signup.jsx
@@ -26,7 +26,7 @@ const Signup = () => {
     e.preventDefault();
     setError(null);
 
-    const response = await fetch('http://localhost:8000/signup/', {
+    const response = await fetch('https://django-1-oiac.onrender.com/signup/', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
