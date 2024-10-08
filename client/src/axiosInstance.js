6c83828227296e38ec4cc23c832e727f0a9c0f42
AnuragOP01 <121534840+AnuragOP01@users.noreply.github.com>
2024-10-08 19:00:35 +0530

    Update axiosInstance.js

--- client/src/axiosInstance.js
+++ client/src/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'https://django-1-oiac.onrender.com/api', // Replace with your API base URL
+  baseURL: 'https://django-1-oiac.onrender.com', // Replace with your API base URL
   timeout : 10000,
   headers : {
     "Content-Type" : "application/json",
